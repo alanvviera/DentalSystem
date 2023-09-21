29a403e9d0d22cadce247d2266f63a7df9751ffc
Miguel <miargote@hotmail.com>
2023-09-21 15:52:46 -0700

    Added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        primary: '#4287ef',
+        secondary: '#F1F7FE',
+        tertiary: '#e0ecfb'
+      }
     },
   },
   plugins: [],
