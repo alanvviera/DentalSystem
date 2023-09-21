78edc8075b2b4e76f97c7a1a8eb37998e3fff3b6
Andre Cadena <al19760606@ite.edu.mx>
2023-09-21 15:59:14 -0700

    Font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Dental System',
