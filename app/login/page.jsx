fb387eb456b4eebfbde76415c8b4e365609247a3
Nerovelic <zack.vadhir2@hotmail.com>
2023-09-21 19:04:59 -0700

    creacion del login

---
+++ app/login/page.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col items-center pt-10 bg-white min-h-screen">
+      <div className="container max-w-md bg-secondary rounded-lg overflow-hidden">
+        <form className="flex flex-col items-center p-8 gap-4 text-center">
+          <div className="head flex flex-col gap-4">
+            <span className="text-2xl font-bold text-black">Login</span>
+            <p className="text-base text-gray-700"></p>
+          </div>
+          <div className="inputs overflow-hidden bg-white w-full my-4 mx-2 p-2 rounded-lg border-b-0">
+            <input
+              type="email"
+              placeholder="Email"
+              className="border-none outline-none w-full h-10 border-b border-gray-300 font-light"
+            />
+          </div>
+          <div className="inputs overflow-hidden bg-white w-full mb-4 mx-2 p-2 rounded-lg border-b-0">
+            <input
+              type="password"
+              placeholder="Password"
+              className="border-none outline-none w-full h-10 border-b border-gray-300 font-light"
+            />
+          </div>
+          <button className="bg-primary text-white w-full h-10 rounded-lg font-semibold cursor-pointer hover:bg-blue-700 transition duration-500">
+            Login
+          </button>
+        </form>
+        <div className="form-footer bg-blue-200 p-4 text-base text-center">
+          <p>
+            Don't have an account yet?{" "}
+            <a
+              href="#"
+              className="font-bold text-blue-600 hover:text-blue-700 transition duration-300"
+            >
+              Create one
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
