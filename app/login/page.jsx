a26471a1bc64342d28e324334fbbb22c32fe35b1
Miguel <miargote@hotmail.com>
2023-09-21 21:21:43 -0700

    Added style css

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,50 +1,39 @@
-import React from "react";
+import Link from "next/link";
+import './style.css';
 
 export default function Login() {
   return (
-    <div className="flex flex-col items-center pt-10 bg-white min-h-screen">
-      <div className="container max-w-md bg-secondary rounded-lg overflow-hidden">
-        <form className="flex flex-col items-center p-8 gap-4 text-center">
-          <div className="head flex flex-col gap-4">
-            <span className="text-2xl font-bold text-black">Log In</span>
-            <p className="text-base text-gray-700"></p>
-          </div>
-          <div className="inputs overflow-hidden bg-white w-full my-4 mx-2 p-2 rounded-lg border-b-0">
-            <input
-              type="email"
-              placeholder="Email"
-              className="border-none outline-none w-full h-10 border-b border-gray-300 font-light text-black"
-            />
-          </div>
-          <div className="inputs overflow-hidden bg-white w-full mb-4 mx-2 p-2 rounded-lg border-b-0">
-            <input
-              type="password"
-              placeholder="Password"
-              className="border-none outline-none w-full h-10 border-b border-gray-300 font-light text-black"
-            />
-          </div>
+    <div>
+      <section>
+        <div className='top-wave'></div>
+        <div className='bottom-wave'></div>
+      </section>
+      <div className="body">
+        <div className="container">
+          <form>
+            <div className="head">
+              <span>Log In</span>
+              <p>With an existing account</p>
+            </div>
+            <div className="inputs">
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+            </div>
+            <div>
+              <a href="#">
+                Forgot your password?
+              </a>
+            </div>
+            <button>Log In</button>
+          </form>
           <div className="form-footer">
-            <a
-              href="#"
-              className="font-bold text-blue-600 hover:text-blue-700 transition duration-300 underline"
-            >
-              Forgot you password?
-            </a>
+            <p>
+              Don't have an account yet?
+              <Link href="/signup">
+                Create one
+              </Link>
+            </p>
           </div>
-          <button className="bg-primary text-white w-full h-10 rounded-lg font-semibold cursor-pointer hover:bg-blue-700 transition duration-500">
-            Log In
-          </button>
-        </form>
-        <div className="form-footer bg-blue-200 p-4 text-base text-center">
-          <p>
-            Don't have an account yet?{" "}
-            <a
-              href="#"
-              className="font-bold text-blue-600 hover:text-blue-700 transition duration-300"
-            >
-              Create one
-            </a>
-          </p>
         </div>
       </div>
     </div>
