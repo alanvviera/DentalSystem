3d905d1235561218580e8bfa79ac0e30a1febbbd
AngelSilent <angelstarkpana@gmail.com>
2023-09-24 01:15:59 -0700

    Validations in fields on the login and sign up pages.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,19 +4,19 @@ import { useForm } from "@/hooks/useForm";
 
 export default function Login() {
 
-  const {email, password, onChange,cleanFields } = useForm({
-    email: "",
-    password: ""
-  }
-  );
+    const { email, password, onChange, cleanFields } = useForm({
+      email: "",
+      password: ""
+    }
+    );
 
 
-  function onSubmit(e) {
-    e.preventDefault();
-    // This is the code that is executed when the validations are correct and the form submit is executed
-    console.log(`This is the email: ${email}, this is the password: ${password}`);
-    cleanFields();
-  }
+    function onSubmit(e) {
+      e.preventDefault();
+      // This is the code that is executed when the validations are correct and the form submit is executed
+      console.log(`This is the email: ${email}, this is the password: ${password}`);
+      cleanFields();
+    }
 
   return (
     <main>
@@ -29,7 +29,7 @@ export default function Login() {
         <article className="bg-secondary max-w-[300px] rounded-2xl overflow-hidden m-auto">
           {" "}
           {/* container */}
-          <form className="flex flex-col items-center justify-center gap-4 text-center pt-8 px-6 pb-6" onSubmit={onSubmit}>
+          <form className="flex flex-col items-center justify-center gap-4 text-center pt-8 px-6 pb-6 group" onSubmit={onSubmit} noValidate>
             <header className="flex flex-col gap-4">
               {" "}
               {/* head */}
@@ -41,27 +41,36 @@ export default function Login() {
             <section className="overflow-hidden bg-[#fff] w-[100%] mx-4 my-2 border-none rounded-lg outline-0">
               {" "}
               {/* inputs */}
-              <input
-                className=" outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c]"
-                type="email"
-                placeholder="Correo electrónico"
-                name="email"
-                onChange={onChange}
-                value={email}
-                required
-                pattern="[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}"
-              />
-              <input
-                className=" outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c]"
-                type="password"
-                placeholder="Contraseña"
-                name="password"
-                value={password}
-                onChange={onChange}
-                minLength={8}
-                required
-
-              />
+              <div>
+                <input
+                  className=" outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c] peer"
+                  type="email"
+                  placeholder="Correo electrónico"
+                  name="email"
+                  onChange={onChange}
+                  value={email}
+                  required
+                  pattern="[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}"
+                />
+                <p className="mt-2 mb-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                  Por favor ingrese un correo válido.
+                </p>
+              </div>
+              <div>
+                <input
+                  className=" outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c] peer"
+                  type="password"
+                  placeholder="Contraseña"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
+                  pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+                  required
+                />
+                  <p className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                  La contraseña debe contener mínimo ocho caracteres, una letra, un número y un carácter especial.
+                </p>
+              </div>
             </section>
             <section>
               <p>
@@ -73,7 +82,7 @@ export default function Login() {
                 </Link>
               </p>
             </section>
-            <button type="submit" className="bg-primary text-white w-full h-10 pt-2 pb-2 border-0 overflow-hidden rounded-3xl text-base font-semibold cursor-pointer transition-all ease-in-out duration-1000 hover:bg-[#005ce6]">
+            <button type="submit" className="bg-primary text-white w-full h-10 pt-2 pb-2 border-0 overflow-hidden rounded-3xl text-base font-semibold cursor-pointer transition-all ease-in-out duration-1000 hover:bg-[#005ce6] group-invalid:pointer-events-none group-invalid:opacity-30">
               Inicia sesión
             </button>
           </form>
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,29 +1,23 @@
 "use client";
-import React, { useState } from 'react';
 import Link from 'next/link';
+import { useForm } from '@/hooks/useForm';
 
 function SignUp() {
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
-    const [passwordValid, setPasswordValid] = useState(true);
+    const { email, password, user, showPassword, onChange, cleanFields } = useForm({
+        email: "",
+        password: "",
+        user: "",
+        showPassword: false
+    }
+    );
 
-    const handleSubmit = (e) => {
+
+    function onSubmit(e) {
         e.preventDefault();
-
-        // Resto del código para enviar el formulario si la validación es exitosa.
-    };
-
-    const validatePassword = (value) => {
-        // Utiliza una expresión regular para validar la contraseña.
-        const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{6,})/;
-        return passwordRegex.test(value);
-    };
-
-    const handlePasswordChange = (e) => {
-        const newPassword = e.target.value;
-        setPassword(newPassword);
-        setPasswordValid(validatePassword(newPassword));
-    };
+        // This is the code that is executed when the validations are correct and the form submit is executed
+        console.log(`This is the email: ${email}, this is the password: ${password}, this is the user: ${user}`);
+        cleanFields();
+    }
 
     return (
         <main>
@@ -33,48 +27,73 @@ function SignUp() {
             </section>
             <section className="flex flex-col justify-center min-h-[100vh]"> {/* body */}
                 <article className="bg-secondary max-w-[300px] rounded-2xl overflow-hidden m-auto"> {/* form container */}
-                    <form className='flex flex-col items-center justify-center gap-4 text-center pt-8 px-6 pb-6' onSubmit={handleSubmit}>
+                    <form className='flex flex-col items-center justify-center gap-4 text-center pt-8 px-6 pb-6 group' onSubmit={onSubmit} noValidate>
                         <header className="flex flex-col gap-4"> {/* head */}
                             <span className="font-bold text-black text-2xl">Regístrate</span>
                             <p className="text-[#7C6666] text-lg">Crea una cuenta gratis con tu correo.</p>
                         </header>
                         <section className="overflow-hidden bg-[#fff] w-[100%] mx-4 my-2 border-none rounded-lg outline-0"> {/* inputs */}
-                            <input
-                                className="outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c]"
-                                type="text"
-                                placeholder="Nombre completo"
-                                required // required field
-                            />
-                            <input
-                                className="outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c]"
-                                type="email"
-                                placeholder="Correo electrónico"
-                                required // required field
-                            />
-                            <section className="relative"> {/* Password field container */}
+                            <div>
                                 <input
-                                    className={`outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-${passwordValid ? '[#8080804c]' : 'red'} pr-10 relative`}
-                                    type={showPassword ? 'text' : 'password'}
-                                    placeholder="Contraseña"
-                                    value={password}
-                                    onChange={handlePasswordChange}
-                                    required // Required field
+                                    className="outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c] peer"
+                                    type="text"
+                                    name="user"
+                                    onChange={onChange}
+                                    value={user}
+                                    placeholder="Nombre completo"
+                                    minLength={4}
+                                    required // required field
                                 />
+                                <p className="mt-2 mb-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                                    Su nombre completo debe de tener minimo 4 caracteres
+                                </p>
+                            </div>
+                            <div>
+                                <input
+                                    className="outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c] peer"
+                                    type="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={onChange}
+                                    placeholder="Correo electrónico"
+                                    pattern="[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}"
+                                    required // required field
+                                />
+                                <p className="mt-2 mb-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                                    Por favor ingrese un correo válido.
+                                </p>
+                            </div>
+                            <section className="relative"> {/* Password field container */}
+                                <div>
+                                    <input
+                                        className={`outline-0 py-2 px-[15px] w-[100%] h-[40px] font-extralight border-b border-solid border-[#8080804c] pr-10 relative peer`}
+                                        type={showPassword ? 'text' : 'password'}
+                                        placeholder="Contraseña"
+                                        pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+                                        name="password"
+                                        value={password}
+                                        onChange={onChange}
+                                        required // Required field
+                                    />
+                                    <p className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                                    La contraseña debe contener mínimo ocho caracteres, una letra, un número y un carácter especial.
+                                    </p>
+                                </div>
                                 <button
                                     type="button"
+                                    name="showPassword"
                                     className="show-password-button"
-                                    onClick={() => setShowPassword(!showPassword)}
+                                    onClick={() => onChange({target: {
+                                        name: "showPassword",
+                                        value: !showPassword
+                                    }})}
                                 >
                                     {showPassword ? '👁️' : '🔒'}
                                 </button>
                             </section>
-                            {!passwordValid && (
-                                <p className="text-red-500 text-xs mt-1">
-                                    La contraseña debe contener al menos 6 caracteres, 1 letra mayúscula y 1 símbolo.
-                                </p>
-                            )}
+                           
                         </section>
-                        <button className='bg-primary text-white w-full h-10 pt-2 pb-2 border-0 overflow-hidden rounded-3xl text-base font-semibold cursor-pointer transition-all ease-in-out duration-1000 hover:bg-[#005ce6]'>Regístrate</button>
+                        <button className='bg-primary text-white w-full h-10 pt-2 pb-2 border-0 overflow-hidden rounded-3xl text-base font-semibold cursor-pointer transition-all ease-in-out duration-1000 hover:bg-[#005ce6] group-invalid:pointer-events-none group-invalid:opacity-30'>Regístrate</button>
                     </form>
                     <section className="bg-tertiary p-4 text-base text-center"> {/* form-footer */}
                         <p>¿Ya tienes una cuenta? <Link href="login/" className="font-bold text-[#0066ff] transition-all ease-in-out duration-3000">Inicia sesión</Link></p>
