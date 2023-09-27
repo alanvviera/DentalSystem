473cf13d247df76e0bb7968b0dc722b596e5af0c
Alanvviera <a.cordova.viera@gmail.com>
2023-09-27 12:45:18 -0700

    Documentar pagina  notf-found en jsdoc

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,23 +1,30 @@
-"use client";
+/**
+ * Componente de página de error 404.
+ * @returns {JSX.Element} Elemento JSX que representa la página de error.
+ */
 import Background from "@/components/Background";
 
 export default function ErrorPage() {
-    return (
-        <main>
-            <Background/>
-            <section className="flex flex-col justify-center min-h-screen bg-secondary lg:bg-transparent">
+  return (
+    <main>
+      <Background />
+      {/* Sección principal */}
+      <section className="flex flex-col justify-center min-h-screen bg-secondary lg:bg-transparent">
+        {/* Contenedor de texto centralizado */}
         <div className="text-center">
+          {/* Título del error */}
           <span className="font-bold text-primary text-8xl">
             Error 404
           </span>
         </div>
+        {/* Espaciado superior para el mensaje */}
         <div className="text-center mt-4">
+          {/* Mensaje de página no encontrada */}
           <span className="font-bold text-primary text-4xl">
             Página no encontrada
           </span>
         </div>
       </section>
-        </main>
-    )
-
+    </main>
+  );
 }
