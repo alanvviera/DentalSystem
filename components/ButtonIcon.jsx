06926640e8500da5068986eeb07ffef83ef6ef2b
Miwi64 <miargote@hotmail.com>
2023-09-28 20:34:53 -0800

    href as optional parameter

--- components/ButtonIcon.jsx
+++ components/ButtonIcon.jsx
@@ -2,16 +2,17 @@ import { FacebookFilled } from '@ant-design/icons'
 import Link from 'next/link'
 import React from 'react'
 
-export const ButtonIcon = ({ text, icon, onClick, href }) => {
+export const ButtonIcon = ({ text, icon, onClick, href = "#" }) => {
     return (
-        <Link 
-        href={href}
-        className='flex items-center bg-[#7899e0] p-2 border rounded-lg' onClick={()=>onClick()}>
-            {
-                icon
-            }
-            <p className='text-white'>{text}</p>
-            
+        <Link
+            href={href}
+            onClick={() => onClick()}>
+            <div className='flex items-center bg-[#7899e0] p-2 border rounded-lg'>
+                {
+                    icon
+                }
+                <p className='text-white'>{text}</p>
+            </div>
         </Link>
     )
 }
