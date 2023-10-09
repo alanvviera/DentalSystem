f16e4291417fbca17bdef356c0c12d1c0632b343
Miwi64 <miargote@hotmail.com>
2023-10-09 13:30:32 -0700

    Removed unused files

--- constans/formPattern.js
+++
@@ -1,9 +0,0 @@
-const patternEmail = "[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,}";
-const patternPassword ="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,30}$";
-const patternUser = "(.{4,})";
-
-export {
-    patternEmail,
-    patternPassword,
-    patternUser
-}
