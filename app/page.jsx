86042a929ea91718f3b379312a328a71c04c6890
AngelSilent <angelstarkpana@gmail.com>
2023-10-11 21:59:27 -0700

    Added dashboard

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,18 @@
 "use client";
+import { CenterLoading } from "@/components/CenterLoading";
+import MenuDashboard from "@/components/dashboard/MenuDashboard";
 import { signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session} = useSession();
+  const { data: session,status } = useSession();
 
   return (
-    <section>
-      {session ? (
-        <section>
-          <h1>{session.user.name}</h1>
-          <button onClick={() => signOut()}>Cerrar sesión</button>
-        </section>
-      ) : (
-        <p>Esperando inicio de sesión</p>
-      )}
-    </section>
+   <>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
+    <MenuDashboard 
+    session={session}
+    />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+   </>
   );
 }
