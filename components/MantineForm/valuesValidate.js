ef69d8636c89d4a1d71163b83954ad1d9fc0e266
AngelSilent <angelstarkpana@gmail.com>
2023-11-12 20:48:50 -0800

    Added validations for inputs

---
+++ components/MantineForm/valuesValidate.js
@@ -0,0 +1,25 @@
+
+const validateName = (value) => {
+    return (value.length < 2 ? 'El nombre debe de tener minimo 5 letras' : null);
+}
+
+const validateEmail = (value) => {
+    return (/^\S+@\S+$/.test(value) ? null : 'Correo invalido');
+}
+
+const validateAge = (value) => {
+    return (value < 18 ? 'Su edad debe de ser mayor a 18 años' : null);
+}
+
+const validatePassword = (value, values) => {
+    return (value !== values.password ? 'Las contraseñas no coinciden' : (value.length === 0 || values.password === 0) ? "Las contraseñas no pueden estar vacias" : null);
+}
+
+
+export {
+    validateName,
+    validateEmail,
+    validateAge,
+    validatePassword
+
+}
