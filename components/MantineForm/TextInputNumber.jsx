7f298829548940eb0702b01ac02831fd09eaa15c
AngelSilent <angelstarkpana@gmail.com>
2023-11-12 20:51:39 -0800

    Added a Mantine text input form from for Number

---
+++ components/MantineForm/TextInputNumber.jsx
@@ -0,0 +1,18 @@
+import { TextInput,NumberInput } from '@mantine/core'
+import React from 'react'
+
+function TextInputNumber({label="Age",placeHolder="Age", valueKey="age",form,extraClassname={}}) {
+  return (
+    <NumberInput
+    mt="sm"
+    label={label}
+    placeholder={placeHolder}
+    min={0}
+    max={99}
+    {...form.getInputProps(valueKey)}
+    {...extraClassname}
+/>
+  )
+}
+
+export default TextInputNumber
