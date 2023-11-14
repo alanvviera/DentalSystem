62f1c5968c1289724d3fa5541aa29084596d49c9
endrost33 <88163879+endrost33@users.noreply.github.com>
2023-11-14 15:57:06 -0800

    SideBarMenuLayout Component Full Mantine

--- components/SideBarComponent/LayoutAndre.jsx
+++ components/SideBarComponent/LayoutAndre.jsx
@@ -1,16 +1,27 @@
 "use client"
 import { useDisclosure } from '@mantine/hooks';
-import { AppShell, Burger, Button, Group, NavLink } from '@mantine/core';
-import { UserOutlined, HomeOutlined, TagsOutlined, FileTextOutlined, SettingOutlined, LogoutOutlined, SearchOutlined } from "@ant-design/icons"
+import { AppShell, Burger, Button, Group, Text, Box, NavLink } from '@mantine/core';
+import { UserOutlined, LogoutOutlined } from "@ant-design/icons"
 import { usePathname } from "next/navigation";
+import Link from 'next/link';
+import { useState } from 'react';
 
 export function CollapseDesktop({children, routes}) {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure(true);
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
   const isActive = (param) => usePathname() === param
+  const [active, setActive] = useState(0);
+  let filter;
+  let tl;
 
-  //Cambiar las rutas segun se ocupen
-  
+  function NamePath(){
+    filter = routes.filter(x => x.url == usePathname())
+    //console.log(filter)
+    filter.map((t)=>{
+      tl = t.title
+      //console.log(tl)
+    })
+  }
 
   return (
     <AppShell
@@ -23,31 +34,38 @@ export function CollapseDesktop({children, routes}) {
       padding="md"
     >
       <AppShell.Header>
-        <Group h="100%" px="md">
+        <Group h="100%" px="md" gap={'lg'}>
           <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
           <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-          <UserOutlined />
+          <UserOutlined mb={5} />
+          {NamePath()}
+          <Text fw={500} size="lg">
+            {tl}
+          </Text>
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p="md">
-      <nav>
-            {routes.map((route, key) => (
-              <a
-                href={route.url}
-                key={key}
-                className={
-                  !isActive(route.url)
-                    ? `flex items-center px-4 py-2 mt-1 text-gray-600 transition-colors
-                                    duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 
-                                    dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700`
-                    : `flex items-center px-4 py-2 mt-1 bg-gray-100 rounded-md
-                                    dark:bg-gray-800 dark:text-gray-200 text-gray-700`
-                }
-              >
-                {route.icon}
-                <span className="mx-4 font-medium">{route.title}</span>
-              </a>
-            ))}
+        <Box>
+          {routes.map((item, index)=>(
+            <NavLink
+              component={Link}
+              key={index}
+              active={index === active}
+              label={item.title}
+              href={item.url}
+              leftSection={item.icon}
+              onClick={()=>setActive(index)}
+            />
+          ))}
+          <Button leftSection={<LogoutOutlined/>} fullWidth={true} variant="outline" color='dark'>Cerrar Sesión</Button>
+        </Box>
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
+
+{/* 
             <button    
                 className={`flex items-center px-4 py-2 mt-1 w-full text-gray-600 transition-colors
                 duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 
@@ -55,9 +73,4 @@ export function CollapseDesktop({children, routes}) {
                 <LogoutOutlined/>
                 <span className="mx-4 font-medium">Cerrar Sesión</span>
             </button>
-          </nav>
-      </AppShell.Navbar>
-      <AppShell.Main>{children}</AppShell.Main>
-    </AppShell>
-  );
-}
+*/}
