e205197c98df867444258b8de9fd265728fe8217
AngelSilent <angelstarkpana@gmail.com>
2023-11-14 22:20:15 -0800

    Example form to update a customer record

---
+++ app/form_example/update/page.jsx
@@ -0,0 +1,50 @@
+"use client"
+import React from 'react'
+import { useSession } from 'next-auth/react';
+import { LoadingScreen } from '@/components/LoadingScreen';
+import MantineForm from '@/components/mantineForm/MantineForm';
+import { allValuesAvailable, validateName } from '@/components/mantineForm/valuesValidate';
+import CustomInputMantine, { typeInputForm } from '@/components/mantineForm/customMantineInput';
+
+const FormExampleUpdate = ({ extraClass = "" }) => {
+
+    const { data: session, status } = useSession();
+    if (status === "loading") return <LoadingScreen />
+
+    return (
+        <div className='flex flex-row w-full justify-center content-center items-center'>
+            <MantineForm
+                extraClass={extraClass}
+                initialValuesForKeys={{ client: "Ricardo arjona", clinic: "Clinica sin dientes", appointmentType: "Extracíon de muelas", nameDentist: "Ricardo milos", date: "25 de julio del 2023", hours: "1:40 PM", description: "Debe de ser atentido por el doctor Similares del consultorio 8" }}
+                validateForKeys={{
+                    client: validateName,
+                    clinic: allValuesAvailable,
+                    appointmentType: allValuesAvailable,
+                    nameDentist: validateName,
+                    date: allValuesAvailable,
+                    hours: allValuesAvailable,
+                    description: allValuesAvailable
+                }}
+                listCustomInputMantine={
+                    [
+                        new CustomInputMantine("Nombre del paciente", "Un nombre", "client", typeInputForm.TEXT),
+                        new CustomInputMantine("Nombre de la clinica", "Clinica", "clinic", typeInputForm.TEXT),
+                        new CustomInputMantine("Tipo de la cita", "Tipo de la cita", "appointmentType", typeInputForm.TEXT),
+                        new CustomInputMantine("Nombre del dentista", "El nombre del dentista", "nameDentist", typeInputForm.TEXT),
+                        new CustomInputMantine("Fecha", "Fecha", "date", typeInputForm.TEXT),
+                        new CustomInputMantine("Hora", "Hora", "hours", typeInputForm.TEXT),
+                        new CustomInputMantine("Descripción", "Descripción de la cita", "description", typeInputForm.TEXT),
+                    ]
+                }
+                title='Actualizar registro'
+                onSubmit={(form) => {
+                    //All fields were validated
+                    console.log(form.values);
+                    form.setInitialValues();
+                }}
+                labelSubmit='Actualizar'
+            />
+        </div>
+    )
+}
+export default FormExampleUpdate
