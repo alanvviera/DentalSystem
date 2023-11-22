58c5f18c84a32abdca4b9ffe9bc477b299f3037c
AngelSilent <angelstarkpana@gmail.com>
2023-11-22 00:04:25 -0800

    Page clinics added

---
+++ app/employee-menu/clinics/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import {Title } from '@mantine/core';
+import VisualizeData from "../../../components/visualize-data/VisualizeData";
+import LabelDataText from "../../../components/visualize-data/labelsData/LabelDataText";
+
+
+  const ClinicPage = () => {
+  return (
+<main style={{ margin: "20px" }}>
+      <VisualizeData
+        content={
+          <>
+            <Title>Clinica</Title>
+            <LabelDataText title='Ve la información de tu clinica' type='Información personal' subIcon={<Title style={{cursor: 'pointer'}} onClick={()=>{console.log("Ir a la pagina")}} order={6} >Editar</Title>} />
+            <LabelDataText title='Ve el personal de tu clinica' type='Personal' subIcon={<Title style={{cursor: 'pointer'}} onClick={()=>{console.log("Ir a la pagina")}} order={6} >Ver mas</Title>} />
+            <LabelDataText title='Manera tu inventario' type='Inventario' subIcon={<Title style={{cursor: 'pointer'}} onClick={()=>{console.log("Ir a la pagina")}} order={6} >Ver mas</Title>} />
+            <LabelDataText title='Administra a tus clientes' type='Clientes' subIcon={<Title style={{cursor: 'pointer'}} onClick={()=>{console.log("Ir a la pagina")}} order={6} >Ver mas</Title>} />
+            <LabelDataText title='Ve las citas pendientes' type='Pendientes' subIcon={<Title style={{cursor: 'pointer'}} onClick={()=>{console.log("Ir a la pagina")}} order={6} >Ver mas</Title>} />
+          </>
+        }
+      />
+    </main>
+  )
+}
+
+export default ClinicPage;
