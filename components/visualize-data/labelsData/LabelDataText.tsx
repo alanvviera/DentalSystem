016793b9dd40be7686180d72596ba182af76764c
AngelSilent <angelstarkpana@gmail.com>
2023-11-22 00:01:44 -0800

    Props Icon added

--- components/visualize-data/labelsData/LabelDataText.tsx
+++ components/visualize-data/labelsData/LabelDataText.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Card, Text, Badge, Group } from '@mantine/core';
 import { DownOutlined } from '@ant-design/icons';
 
 type LabelDataTextProps = {
   type?: string;
   title?: string;
+  subIcon?: ReactNode
 }
 
-const LabelDataText = ({ type = "", title = "" }:LabelDataTextProps) => (
+const LabelDataText = ({ type = "", title = "", subIcon= <DownOutlined /> }:LabelDataTextProps) => (
     <Card shadow="sm" padding="sm" radius="md" withBorder className='m-3'>
       <Group justify="space-between">
         <Text fw={500} size='sm'>
           {type}
         </Text>
-        <Badge color="blue" variant="light">
-          <DownOutlined />
-        </Badge>
+        <div className='text-[#228BE6] bg-[#E8F3FC] px-[8px] py-[2px] rounded-lg'>
+         {subIcon}
+        </div>
       </Group>
       <Text size="sm" c="dimmed">
         {title}
