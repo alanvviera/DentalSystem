e30463f27446df9e8f159d4e0cd2e18062abddac
AngelSilent <angelstarkpana@gmail.com>
2023-11-21 22:46:03 -0800

    Pattern date and pattern tel added

--- constants/formPattern.ts
+++ constants/formPattern.ts
@@ -1,9 +1,13 @@
 const patternEmail = "[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,}";
 const patternPassword ="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,30}$";
 const patternUser = "(.{4,})";
+const patternNumberTel = "[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}";
+const patternDate = "^(((0[1-9]|[12]\\d|3[01])\\/(0[13578]|1[02])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)\\/(0[13456789]|1[012])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\/02\\/((19|[2-9]\\d)\\d{2}))|(29\\/02\\/((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))$";
 
 export {
     patternEmail,
     patternPassword,
-    patternUser
+    patternUser,
+    patternNumberTel,
+    patternDate
 }
