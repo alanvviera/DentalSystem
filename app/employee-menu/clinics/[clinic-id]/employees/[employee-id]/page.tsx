e636779327798a83d6b2af32ee2c07dc337b04b9
AngelSilent <angelstarkpana@gmail.com>
2023-11-22 19:57:25 -0800

    Added button delete in view employee page

--- app/employee-menu/clinics/[clinic-id]/employees/[employee-id]/page.tsx
+++ app/employee-menu/clinics/[clinic-id]/employees/[employee-id]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React from 'react';
-
-import { Button, Title } from "@mantine/core";
-import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Button, Flex, Text, Title } from "@mantine/core";
+import { ArrowLeftOutlined, DeleteFilled } from "@ant-design/icons";
 import MantineForm from '../../../../../../components/mantine-form/MantineForm';
 import { validaFieldsNotEmpty, validateDate, validateEmail, validateNumberInteger, validateNumberTel } from '../../../../../../components/mantine-form/valuesValidate';
 import CustomInputMantine, { typeInputForm } from '../../../../../../components/mantine-form/customMantineInput';
+import { modals, } from '@mantine/modals';
 
 const getData = async (employeeId: String) => {
     return {
@@ -29,7 +29,6 @@ const EditClinicsPage = async ({ params }) => {
 
     //Data Example
     const dataEmployeeExample = await getData(employeeId);
-
     //OnSubmit
     const onSubmit = (form: any) => {
         // All fields were validated
@@ -37,53 +36,77 @@ const EditClinicsPage = async ({ params }) => {
         form.setInitialValues();
     }
 
-    return (
-        <main style={{ margin: "20px" }}>
-            <Button px={0} component="a" leftSection={<ArrowLeftOutlined />} variant="subtle" href={`/employee-menu/clinics/${clinicId}/employees`} >
-                Volver a personal
-            </Button>
-            <Title>Información del empleado</Title>
-            <MantineForm
-                initialValuesForKeys={{
-                    ...dataEmployeeExample
+    const onDelete = ()=>{
+        console.log(`Delete employee with id ${employeeId}`);
+    }
 
-                }}
-                validateForKeys={{
-                    name: validaFieldsNotEmpty,
-                    email: validateEmail,
-                    numTel: validateNumberTel,
-                    curp: validaFieldsNotEmpty,
-                    activity: validaFieldsNotEmpty,
-                    medicalDegree: validaFieldsNotEmpty,
-                    addrress: validaFieldsNotEmpty,
-                    dateBirthday: validateDate,
-                    gender: validaFieldsNotEmpty,
-                    state: validaFieldsNotEmpty,
-                    speciality: validaFieldsNotEmpty
-                }}
-                listCustomInputMantine={[
-                    //1- Title, 2- Subtile, 3-Is this same key that in validateForKeys, 4- TypeText
-                    new CustomInputMantine("Nombre completo", "Nombre completo", "name", typeInputForm.TEXT),
-                    new CustomInputMantine("Correo electrónico", "Correo electrónico", "email", typeInputForm.TEXT),
-                    new CustomInputMantine("Número de teléfono", "Ejem: 646-123-45-67", "numTel", typeInputForm.TEXT),
-                    new CustomInputMantine("CURP", "Ingrese el curp", "curp", typeInputForm.TEXT),
-                    new CustomInputMantine("Cargo", "Cargo", "activity", typeInputForm.TEXT),
-                    new CustomInputMantine("Licencia medica", "Licencia medica", "medicalDegree", typeInputForm.TEXT),
-                    new CustomInputMantine("Dirección", "Dirección", "addrress", typeInputForm.TEXT),
-                    new CustomInputMantine("Fecha de nacimiento", "Fecha de nacimiento", "dateBirthday", typeInputForm.DATEPICKER, new Date(Number(dataEmployeeExample.dateBirthday))),
-                    new CustomInputMantine("Genero", "Genero", "gender", typeInputForm.TEXT),
-                    new CustomInputMantine("Lugar de estado", "Lugar de estado", "state", typeInputForm.TEXT),
-                    new CustomInputMantine("Especialidad", "Especialidad", "speciality", typeInputForm.TEXT),
-                ]}
-                onSubmit={(form: any) => {
-                    onSubmit(form);
-                }}
-                labelSubmit='Guardar cambios'
-            />
-            <Button variant="default" component="a" href={`/employee-menu/clinics/${clinicId}/employees/`} fullWidth mt={10}>
-                Cancelar
-            </Button>
-        </main>
+    const openDeleteModal = () =>
+        modals.openConfirmModal({
+            title: 'Estas seguro de eliminar a este empleado',
+            centered: true,
+            children: (
+                <Text size="sm">
+                   Esta opción no es reversible
+                </Text>
+            ),
+            labels: { confirm: 'Eliminar empleado', cancel: "Cancelar" },
+            confirmProps: { color: 'red' },
+            onCancel: () => {},
+            onConfirm: () => onDelete(),
+        });
+
+    return (
+            <main style={{ margin: "20px" }}>
+                <Flex justify={"space-between"}>
+                    <Button px={0} component="a" leftSection={<ArrowLeftOutlined />} variant="subtle" href={`/employee-menu/clinics/${clinicId}/employees`} >
+                        Volver a personal
+                    </Button>
+                    <Button justify="center" color='red' px={0} component="a" rightSection={<DeleteFilled />} variant="filled" pl={"xs"} pr={"xs"} onClick={openDeleteModal} >
+                        Eliminar
+                    </Button>
+                </Flex>
+                <Title>Información del empleado</Title>
+                <MantineForm
+                    initialValuesForKeys={{
+                        ...dataEmployeeExample
+
+                    }}
+                    validateForKeys={{
+                        name: validaFieldsNotEmpty,
+                        email: validateEmail,
+                        numTel: validateNumberTel,
+                        curp: validaFieldsNotEmpty,
+                        activity: validaFieldsNotEmpty,
+                        medicalDegree: validaFieldsNotEmpty,
+                        addrress: validaFieldsNotEmpty,
+                        dateBirthday: validateDate,
+                        gender: validaFieldsNotEmpty,
+                        state: validaFieldsNotEmpty,
+                        speciality: validaFieldsNotEmpty
+                    }}
+                    listCustomInputMantine={[
+                        //1- Title, 2- Subtile, 3-Is this same key that in validateForKeys, 4- TypeText
+                        new CustomInputMantine("Nombre completo", "Nombre completo", "name", typeInputForm.TEXT),
+                        new CustomInputMantine("Correo electrónico", "Correo electrónico", "email", typeInputForm.TEXT),
+                        new CustomInputMantine("Número de teléfono", "Ejem: 646-123-45-67", "numTel", typeInputForm.TEXT),
+                        new CustomInputMantine("CURP", "Ingrese el curp", "curp", typeInputForm.TEXT),
+                        new CustomInputMantine("Cargo", "Cargo", "activity", typeInputForm.TEXT),
+                        new CustomInputMantine("Licencia medica", "Licencia medica", "medicalDegree", typeInputForm.TEXT),
+                        new CustomInputMantine("Dirección", "Dirección", "addrress", typeInputForm.TEXT),
+                        new CustomInputMantine("Fecha de nacimiento", "Fecha de nacimiento", "dateBirthday", typeInputForm.DATEPICKER, new Date(Number(dataEmployeeExample.dateBirthday))),
+                        new CustomInputMantine("Genero", "Genero", "gender", typeInputForm.TEXT),
+                        new CustomInputMantine("Lugar de estado", "Lugar de estado", "state", typeInputForm.TEXT),
+                        new CustomInputMantine("Especialidad", "Especialidad", "speciality", typeInputForm.TEXT),
+                    ]}
+                    onSubmit={(form: any) => {
+                        onSubmit(form);
+                    }}
+                    labelSubmit='Guardar cambios'
+                />
+                <Button variant="default" component="a" href={`/employee-menu/clinics/${clinicId}/employees/`} fullWidth mt={10}>
+                    Cancelar
+                </Button>
+            </main>
     );
 };
 
