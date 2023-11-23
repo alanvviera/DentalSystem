69faee622fadb8ddb2fb554033465f2cea2a1eaf
AngelSilent <angelstarkpana@gmail.com>
2023-11-23 15:46:45 -0800

    Inventory page added

--- app/employee-menu/clinics/page.tsx
+++ app/employee-menu/clinics/page.tsx
@@ -19,7 +19,7 @@ const ClinicPage = () => {
                         <Title>Clinica</Title>
                         <LabelDataText title='Ve la información de tu clinica' type='Información personal' subIcon={<Title style={{ cursor: 'pointer' }} onClick={() => router.push(`/employee-menu/clinics/${clinicId}`)} order={6} >Editar</Title>} />
                         <LabelDataText title='Ve el personal de tu clinica' type='Personal' subIcon={<Title style={{ cursor: 'pointer' }} onClick={() => router.push(`/employee-menu/clinics/${clinicId}/employees`)} order={6} >Ver mas</Title>} />
-                        <LabelDataText title='Manera tu inventario' type='Inventario' subIcon={<Title style={{ cursor: 'pointer' }} onClick={() => router.push(`/employee-menu/clinics/${clinicId}`)} order={6} >Ver mas</Title>} />
+                        <LabelDataText title='Manera tu inventario' type='Inventario' subIcon={<Title style={{ cursor: 'pointer' }} onClick={() => router.push(`/employee-menu/clinics/${clinicId}/inventory`)} order={6} >Ver mas</Title>} />
                         <LabelDataText title='Administra a tus clientes' type='Clientes' subIcon={<Title style={{ cursor: 'pointer' }} onClick={() => router.push(`/employee-menu/clinics/${clinicId}`)} order={6} >Ver mas</Title>} />
                         <LabelDataText title='Ve las citas pendientes' type='Pendientes' subIcon={<Title style={{ cursor: 'pointer' }} onClick={() => router.push(`/employee-menu/clinics/${clinicId}`)} order={6} >Ver mas</Title>} />
                     </>
