1b440c3e445557e2b0dadd97047205c328cde692
AngelSilent <angelstarkpana@gmail.com>
2023-12-04 20:42:41 -0800

    Added clinics page with server session

--- app/menu/clinics/page.tsx
+++ app/menu/clinics/page.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import ClinicsAll from '../../../components/employee-menu/clinics/ClinicsAll';
 import fakeTypeUser from '../../../constants/fakeTypeUser';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]/route';
+import SkeletonForm from '../../../components/custom-skeleton/SkeletonForm';
 
 
-const page = () => {
+const page =async () => {
     
-    const typeUser = fakeTypeUser;
+    const session = await getServerSession(authOptions);
+    const { user: { name, email, type_user } } = session;
 
+    if (!session) {
+        return <SkeletonForm />
+    }
+
+    const typeUser = session.user?.type_user;
 
     if (typeUser === "EMPLOYEE") {
         return <ClinicsAll />
