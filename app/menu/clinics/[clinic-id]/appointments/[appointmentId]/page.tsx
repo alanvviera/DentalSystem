4fc7479550117bb5e938d78acb2fa161084b2705
AngelSilent <angelstarkpana@gmail.com>
2023-12-04 20:26:04 -0800

    Added clinic appointment pages with server session

--- app/menu/clinics/[clinic-id]/appointments/[appointmentId]/page.tsx
+++ app/menu/clinics/[clinic-id]/appointments/[appointmentId]/page.tsx
@@ -3,14 +3,23 @@ import { useRouter } from 'next/navigation';
 
 import ClinicAppointmentId from '../../../../../../components/employee-menu/clinics/[clinic-id]/appointments/[appointment-id]/ClinicAppointmentId';
 import fakeTypeUser from '../../../../../../constants/fakeTypeUser';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../../../../api/auth/[...nextauth]/route';
+import SkeletonForm from '../../../../../../components/custom-skeleton/SkeletonForm';
 
 
-const page = ({ params }) => {
-    
+const page = async ({ params }) => {
+
     const clinicId = params["clinic-id"];
     const appointmentId = params["appointment-id"];
-    const typeUser = fakeTypeUser;
 
+    const session = await getServerSession(authOptions);
+    const { user: { name, email, type_user } } = session;
+
+    if (!session) {
+        return <SkeletonForm />
+    }
+    const typeUser = session.user?.type_user;
 
     if (typeUser === "EMPLOYEE") {
         return <ClinicAppointmentId clinicId={clinicId} appointmentId={appointmentId} />
