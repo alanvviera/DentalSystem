c8c7b456c48cf4077f22cfb56abd2e6eaf4c8f16
AngelSilent <angelstarkpana@gmail.com>
2023-12-04 20:56:44 -0800

    Added medical history pages with server session

--- app/menu/medicalHistory/[medicalHistoryId]/page.tsx
+++ app/menu/medicalHistory/[medicalHistoryId]/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ClientMedicalHistoryId from '../../../../components/client-menu/medicalHistory/[medicalHistoryId]/page';
-import fakeTypeUser from '../../../../constants/fakeTypeUser';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../../api/auth/[...nextauth]/route';
+import SkeletonForm from '../../../../components/custom-skeleton/SkeletonForm';
 
 
-const page = () => {
-    const typeUser = fakeTypeUser;
+const page =async () => {
+    const session = await getServerSession(authOptions);
+    const { user: { name, email, type_user } } = session;
 
+    if (!session) {
+        return <SkeletonForm />
+    }
+
+    const typeUser = session.user?.type_user;
 
 
     if (typeUser === "CLIENT") {
