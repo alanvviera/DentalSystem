b45165a570f798ebafc453b03f788570c2b48c33
AngelSilent <angelstarkpana@gmail.com>
2023-12-04 20:51:43 -0800

    Added debt pages with server session

--- app/menu/debt/page.tsx
+++ app/menu/debt/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import ClientDashboard from '../../../components/client-menu/dashboard/ClientDashboard';
 import ClientDebt from '../../../components/client-menu/debt/ClientDebt';
-import fakeTypeUser from '../../../constants/fakeTypeUser';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]/route';
+import SkeletonForm from '../../../components/custom-skeleton/SkeletonForm';
 
 
-const page = () => {
-    const typeUser = fakeTypeUser;
+const page =async () => {
+    const session = await getServerSession(authOptions);
+    const { user: { name, email, type_user } } = session;
+
+    if (!session) {
+        return <SkeletonForm />
+    }
+
+    const typeUser = session.user?.type_user;
 
     if (typeUser === "CLIENT") {
         return <ClientDebt />
