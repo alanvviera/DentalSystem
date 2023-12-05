b86338d6d9dbd40ce77307fc3c64c3f2a8a385cf
CesatSoftware1 <software1@cesat.mx>
2023-12-04 18:18:47 -0800

    test fetch of ClientAppointment page

--- components/client-menu/appointments/ClientAppointments.tsx
+++ components/client-menu/appointments/ClientAppointments.tsx
@@ -1,4 +1,102 @@
+import React, { useEffect, useState } from "react";
 import { Grid, GridCol, Text } from "@mantine/core";
+import TilesSection from "../../tiles-viewer/TileSection";
+import CustomTile from "../../tiles-viewer/CustomTile";
+import { Title, Group, Button } from "@mantine/core";
+import { PlusOutlined } from "@ant-design/icons";
+
+type Appoint = {
+  id: number;
+  descripcion?: string;
+  fecha: string;
+  hora: string;
+  doctor_name: string;
+};
+
+const ClientAppointments = () => {
+  const [appointmentsPending, setAppointmentsPending] = useState<Appoint[]>([]);
+  const [appointmentsCompleted, setAppointmentsCompleted] = useState<Appoint[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/clientcheckappointment"); // Reemplaza "/tu-endpoint" con la URL correcta
+        const data = await response.json();
+        setAppointmentsPending(data.appointmentsPending);
+        setAppointmentsCompleted(data.appointmentsCompleted);
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // El array vacío [] asegura que la solicitud se realice solo una vez al montar el componente
+
+  return (
+    <div style={{ margin: "20px" }}>
+      <Group justify="space-between">
+        <Title>Citas</Title>
+        <Button
+          leftSection={<PlusOutlined />}
+          component="a"
+          href="/menu/appointments/create"
+        >
+          Agregar
+        </Button>
+      </Group>
+      <TilesSection title="Pendientes" />
+      <Grid>
+        {appointmentsPending.map((appt: Appoint, index: number) => (
+          <GridCol span={12} key={index}>
+            <CustomTile
+              key={index}
+              title={appt.descripcion || ""}
+              baseLink={`/menu/appointments/${appt.id}`}
+            >
+              <Text size="md">
+                <strong>Fecha:</strong> {appt.fecha}
+              </Text>
+              <Text size="md">
+                <strong>Hora:</strong> {appt.hora}
+              </Text>
+              <Text size="md">
+                <strong>Doctor Asignado:</strong> {appt.doctor_name}
+              </Text>
+            </CustomTile>
+          </GridCol>
+        ))}
+      </Grid>
+      <TilesSection title="Realizadas" />
+      <Grid>
+        {appointmentsCompleted.map((appt: Appoint, index: number) => (
+          <GridCol span={12} key={index}>
+            <CustomTile
+              key={index}
+              title={appt.descripcion || ""}
+              baseLink={`/menu/appointments/${appt.id}`}
+            >
+              <Text size="md">
+                <strong>Fecha:</strong> {appt.fecha}
+              </Text>
+              <Text size="md">
+                <strong>Hora:</strong> {appt.hora}
+              </Text>
+              <Text size="md">
+                <strong>Doctor Asignado:</strong> {appt.doctor_name}
+              </Text>
+            </CustomTile>
+          </GridCol>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default ClientAppointments;
+
+
+
+/*import { Grid, GridCol, Text } from "@mantine/core";
 import React from "react";
 import TilesSection from "../../tiles-viewer/TileSection";
 import CustomTile from "../../tiles-viewer/CustomTile";
@@ -90,3 +188,4 @@ const ClientAppointments = () => {
 };
   
 export default ClientAppointments;
+*/
