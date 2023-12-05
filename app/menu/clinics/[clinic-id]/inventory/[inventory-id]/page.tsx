c2a69e16037d18f2b5cdc9fc2ba925cadf9b8ad9
AngelSilent <angelstarkpana@gmail.com>
2023-12-04 20:37:58 -0800

    Added inventory pages with server session

--- app/menu/clinics/[clinic-id]/inventory/[inventory-id]/page.tsx
+++ app/menu/clinics/[clinic-id]/inventory/[inventory-id]/page.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import ClinicInventoryId from '../../../../../../components/employee-menu/clinics/[clinic-id]/inventory/[inventory-id]/page';
 import fakeTypeUser from '../../../../../../constants/fakeTypeUser';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../../../../api/auth/[...nextauth]/route';
+import SkeletonForm from '../../../../../../components/custom-skeleton/SkeletonForm';
 
 
-const page = ({ params }) => {
+const page = async({ params }) => {
     
     const clinicId = params["clinic-id"];
     const inventoryId = params["inventory-id"];
-    const typeUser = fakeTypeUser;
 
+    const session = await getServerSession(authOptions);
+    const { user: { name, email, type_user } } = session;
+
+    if (!session) {
+        return <SkeletonForm />
+    }
+
+    const typeUser = session.user?.type_user;
     if (typeUser === "EMPLOYEE") {
         return <ClinicInventoryId clinicId={clinicId} inventoryId={inventoryId} />
     }
