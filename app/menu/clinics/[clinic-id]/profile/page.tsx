82ce0c1714f67b1f5e430abcf2d8226ba9c4f4f9
AngelSilent <angelstarkpana@gmail.com>
2023-12-04 20:39:28 -0800

    Added profile page with server session

--- app/menu/clinics/[clinic-id]/profile/page.tsx
+++ app/menu/clinics/[clinic-id]/profile/page.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import ClinicProfile from '../../../../../components/employee-menu/clinics/[clinic-id]/profile/ClinicProfile';
 import fakeTypeUser from '../../../../../constants/fakeTypeUser';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../../../api/auth/[...nextauth]/route';
+import SkeletonForm from '../../../../../components/custom-skeleton/SkeletonForm';
 
 
-const page = ({ params }) => {
+const page =async ({ params }) => {
     
     const clinicId = params["clinic-id"];
-    const typeUser = fakeTypeUser;
+    const session = await getServerSession(authOptions);
+    const { user: { name, email, type_user } } = session;
+
+    if (!session) {
+        return <SkeletonForm />
+    }
+
+    const typeUser = session.user?.type_user;
 
     if (typeUser === "EMPLOYEE") {
         return <ClinicProfile clinicId={clinicId} />
