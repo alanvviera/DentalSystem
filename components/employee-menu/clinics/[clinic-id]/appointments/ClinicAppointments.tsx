c072678d9bb3ef707c513e5eeac3f5b53f2eab9a
AngelSilent <angelstarkpana@gmail.com>
2023-12-05 16:54:56 -0800

    Fixed data appointmentsId page

--- components/employee-menu/clinics/[clinic-id]/appointments/ClinicAppointments.tsx
+++ components/employee-menu/clinics/[clinic-id]/appointments/ClinicAppointments.tsx
@@ -10,22 +10,22 @@ import { dateToString } from '../../../../../utils/parseDates';
 const getData = async () => {
     return [
         {
-            idClient: "238umj02x214db23",
+            idAppointment: "238umj02x214db23",
             name: "José Gonzalez",
             date: new Date(1701996588000)
         },
         {
-            idClient: "2893u9u3v2544a23",
+            idAppointment: "2893u9u3v2544a23",
             name: "Alicia Martinez",
             date: new Date(1702018188000)
         },
         {
-            idClient: "2652b23adw424453",
+            idAppointment: "2652b23adw424453",
             name: "María Gomez",
             date: new Date(1702099848000)
         },
         {
-            idClient: "34dv5645323412s",
+            idAppointment: "34dv5645323412s",
             name: "Martín Suarez",
             date: new Date(1702177200000)
         }
@@ -56,11 +56,11 @@ const ClinicAppointments = async ({ clinicId }) => {
                     <>
                         {
                             listEmployeesExample.map((dataEmployee) => {
-                                return (<LabelDataText  key={dataEmployee.idClient} subTileIcon={<>
+                                return (<LabelDataText  key={dataEmployee.idAppointment} subTileIcon={<>
                                     <p className='font-normal text-sm'>{`Fecha: ${dateToString(dataEmployee.date)}`}</p>
                                     <Space w={12} />
                                 </>} type={`Cliente: ${dataEmployee.name}`} subIcon={
-                                    <Title style={{ cursor: 'pointer' }} onClick={() => router.push(`/menu/clinics/${clinicId}/appointments/${dataEmployee.idClient}`)} order={6} >Editar cita</Title>
+                                    <Title style={{ cursor: 'pointer' }} onClick={() => router.push(`/menu/clinics/${clinicId}/appointments/${dataEmployee.idAppointment}`)} order={6} >Editar cita</Title>
                                 } />
                                 )
 
