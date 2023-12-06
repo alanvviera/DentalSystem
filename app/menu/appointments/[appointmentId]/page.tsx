0dbb0a7ca6aec5d5a9adc60aeecd9461355e00fe
AngelSilent <angelstarkpana@gmail.com>
2023-12-05 17:32:13 -0800

    Added doctor typeUser in pages

--- app/menu/appointments/[appointmentId]/page.tsx
+++ app/menu/appointments/[appointmentId]/page.tsx
@@ -18,7 +18,7 @@ const page = async ({params}) => {
 
     const typeUser = session.user?.type_user;
 
-    if (typeUser === "EMPLOYEE") {
+    if (typeUser === "EMPLOYEE" || typeUser === "DOCTOR") {
         return <EmployeeAppointmentId params={{ appointmentId: 1 }} />
     }
     else {
--- app/menu/clinics/[clinic-id]/clients/[client-id]/page.tsx
+++ app/menu/clinics/[clinic-id]/clients/[client-id]/page.tsx
@@ -20,7 +20,7 @@ const page = async ({ params }) => {
 
     const typeUser = session.user?.type_user;
 
-    if (typeUser === "EMPLOYEE") {
+    if (typeUser === "EMPLOYEE" || typeUser === "DOCTOR") {
         return <ClientId clinicId={clinicId} clientId={clientId} />
     }
     else {
