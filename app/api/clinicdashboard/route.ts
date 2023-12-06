6848bdc5d16b0750152a9ef45d26e4f4a26f1456
Waldo <oswaldo.ct18@gmail.com>
2023-12-05 18:49:42 -0700

    Half of clinic endpoints and a client endpoint updated

--- app/api/clinicdashboard/route.ts
+++ app/api/clinicdashboard/route.ts
@@ -7,10 +7,38 @@ const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest){
 
-    const dashboardData = await prisma.local.findMany{
-        where:{
-            
+    const session = await getServerSession(authOptions);
+
+    try {
+
+        const dashboardData = await prisma.doctor.findMany({
+          where: {
+            //Cambiar employee por doctor cuando ya este la conexion
+            id_user_FK : session.user.id
+          },
+          select:{
+            local:{
+              select:{
+                id: true,
+                name: true,
+                address: true,
+                building_number: true,
+                phone_number:true
+              }
+            }
+          }
+        });
+    
+        if (!dashboardData) {
+          return NextResponse.json({ error: 'Dashboard data not found' }, { status: 404});
         }
-    }
+    
+        return NextResponse.json({dashboardData}, { status: 200 });
+    
+      } catch (error) {
+        console.error({ errorString: 'Error fetching dashboard data: ', error});
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+      }
+    
 
 }
